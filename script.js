10fb1b01c7d30e6429250f5ece19be778e2f49c5
Vrajnish18 <agheravrajnish188@gmail.com>
2025-06-02 21:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function signIn() {
+  alert("Thank you for signing in!");
+}
+
+function addToCart() {
+  alert("glasses has been added to your cart!");
+}
+
+function viewCart() {
+  alert("Viewing your cart!");
+}
